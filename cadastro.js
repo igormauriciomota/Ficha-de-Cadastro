c3ff6517916e13c6bef029fa28e3d22e9a84fba5
Igor Mota <107084301+igormauriciomota@users.noreply.github.com>
2024-03-01 16:59:56 -0300

    segundo commit 01-03-24
    inicio co js

---
+++ cadastro.js
@@ -0,0 +1,11 @@
+let form = document.getElementById('register-form');
+let submit = document.getElementById('btn-submit');
+
+//evento que dispara as validaçoes
+submit.addEventListener('click', function(e) {
+
+    e.preventDefault();
+
+    console.log('funcionou');
+
+});
