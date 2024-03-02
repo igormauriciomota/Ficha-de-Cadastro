a0e1752f0c0719fda9ebe6dc7301a2e4cd2555fa
Igor Mota <107084301+igormauriciomota@users.noreply.github.com>
2024-03-02 20:46:30 -0300

    cadastro js

--- cadastro.js
+++ cadastro.js
@@ -1,11 +1,41 @@
-let form = document.getElementById('register-form');
-let submit = document.getElementById('btn-submit');
+class Validator {
+
+    constructor() {
+        this.validations = [
+
+        ]
+    }
+
+    //iniciar a validação de todos os campos
+    validate(form) {
+        
+        //pegar os inputs
+        let inputs = form.getElementsByTagName('input');
+
+
+        //transformar uma HTMLCollection -> array
+        let inputsArray = [...inputs];
+
+        //loop nos inputs e validação mediante ao que for encontrado
+        inputsArray.forEach(function(input) {
+            console.log(input);
+        });
+
+    }
+
+}
+
+
+let form = document.getElementById("register-form");
+let submit = document.getElementById("btn-submit");
+
+let Validator = new Validator();
 
 //evento que dispara as validaçoes
 submit.addEventListener('click', function(e) {
 
     e.preventDefault();
 
-    console.log('funcionou');
+    Validator.validate(form);
 
 });
